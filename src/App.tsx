5485e663ff3094f66efcf17198e5a6a603a5e7f1
CareteenL <15074806497@163.com>
2020-08-19 11:41:24 +0800

    feat: 🎸 add button example

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { GlobalStyle, Button } from '@careteen/lan';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="Lib" href="https://github.com/careteenL/lan" target="_blank" rel="noopener noreferrer">@careteen/lan</a>
+        <GlobalStyle />
+        <Button>你来点我啊</Button>
       </header>
     </div>
   );
